045531608b50bc4428d46a31dd586b7694267afe
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-29 13:18:52 -0300

    add mongoid to gemfile and create a config for it

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Triresults
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Bootstrap mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
